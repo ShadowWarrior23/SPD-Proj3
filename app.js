47a7f1d024b88ef85aff9ff4c89ecbdbee5c0ba4
x <x@gmail.com>
2024-11-15 18:51:34 +0100

    md-kezdet és nav pofozgatások

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const filter = document.querySelector('.bi-funnel');
-const types = document.querySelector('.types');
+const types = document.querySelector('#types');
+const menuLine = document.querySelector('#menuline');
 filter.addEventListener('click', event => {
-    types.classList.toggle('active');
     types.classList.toggle('hidden');
+    menuLine.classList.toggle('hidden');
+    filter.classList.toggle('bi-funnel-fill');
+    filter.classList.toggle('bi-funnel');
 })
