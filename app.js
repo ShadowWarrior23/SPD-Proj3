86c0ccd20eb3698a193b683178f7fa19fbe14f6c
Domán <domanmartin07@gmail.com>
2024-11-17 00:40:37 +0100

    hero és about ötlet

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ window.addEventListener("load", () => {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
+
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+})
+
+sr.reveal('.home-image',{delay:350, origin:'right'})
+sr.reveal('.about-text-content',{delay:350, origin:'right'})
+sr.reveal('#about-image',{delay:350, origin:'left'})
