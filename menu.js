a20b319caaa38f4c60de1c2ca0586ce800ff9ce4
x <x@gmail.com>
2024-11-17 12:58:11 +0100

    JS_adj

--- menu.js
+++ menu.js
@@ -5,8 +5,8 @@ const filter0 = document.querySelector('#funnel');
 const types = document.querySelector('#types');
 const menuLine = document.querySelector('#menuline');
 filter0.addEventListener('click', event => {
-    types.classList.toggle('hidden');
-    menuLine.classList.toggle('hidden');
+    //types.classList.toggle('hidden');
+    //menuLine.classList.toggle('hidden');
     filter.classList.toggle('bi-funnel-fill');
     filter.classList.toggle('bi-funnel');
 })
