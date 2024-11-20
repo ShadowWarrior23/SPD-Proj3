3f1339d16e0e529bf089d72408f89d6aaa797f1a
x <x@gmail.com>
2024-11-20 12:37:39 +0100

    jav0

--- menu.js
+++ menu.js
@@ -1,17 +1,3 @@
-//Szűrés:
-
-const filter = document.querySelector('.bi-funnel');
-const filter0 = document.querySelector('#funnel');
-const types = document.querySelector('#types');
-const menuLine = document.querySelector('#menuline');
-filter0.addEventListener('click', event => {
-    //types.classList.toggle('hidden');
-    //menuLine.classList.toggle('hidden');
-    filter.classList.toggle('bi-funnel-fill');
-    filter.classList.toggle('bi-funnel');
-})
-
-
 //Kosár:
 
 const cart0 = document.querySelector('#cartOpen0');
