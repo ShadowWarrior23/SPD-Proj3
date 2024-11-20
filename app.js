83a2c67aa78738bcc13c3cca3f6652443d78bd2e
c <c@gmail.com>
2024-11-20 14:19:57 +0100

    finish at home

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const bigCardsDiv = document.querySelector(".big-cards");
+for (let i = 1; i <= 28; i++) {
+    const div = document.createElement("div");
+    div.classList.add("big-card", `big-card${i}`, "hidden");
+    const btn = document.createElement("button");
+    btn.innerText = "x";
+    div.appendChild(btn);
+    const img = document.createElement("img");
+    img.src = `img/img${i}.png`;
+    div.appendChild(img);
+    const p = document.createElement("p");
+    p.id = `p${i}`;
+    div.appendChild(p);
+    const a = document.createElement("a");
+    a.href = "#";
+    a.innerHTML = `<i class="bi bi-cart4 addCart"></i>`;
+    div.appendChild(a);
+    bigCardsDiv.appendChild(div);
+    const overlay = document.createElement("div");
+    overlay.id = `overlay${i}`
+    overlay.classList.add("overlay", "hidden");
+    bigCardsDiv.appendChild(overlay);
+}
+
 //Szűrés:
 
 const filter = document.querySelector('.bi-funnel');
@@ -21,19 +45,22 @@ cart0.addEventListener('click', event => {
     cart.classList.toggle('bi-cart4');
 })
 
-const app1 = document.querySelector("#app1");
+const details = document.querySelectorAll(".details")
+details.forEach (i => {
+    
+})
+
 const bigCard1 = document.querySelector(".big-card1");
-const x = document.querySelector(".big-card1 button");
-const overlay = document.querySelector("#overlay");
+const x1 = document.querySelector(".big-card1 button");
+const overlay1 = document.querySelector("#overlay1");
 app1.addEventListener("click", () => {
     bigCard1.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
+    overlay1.classList.toggle("hidden");
     document.body.style.overflow = 'hidden';
 })
 
-x.addEventListener("click", () => {
-    bigCard1.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
+x1.addEventListener("click", () => {
+   bigCard1.classList.toggle("hidden");
+    overlay1.classList.toggle("hidden");
     document.body.style.overflow = '';
 })
-
