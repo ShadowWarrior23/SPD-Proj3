5eb8fe6258e0c84c0bc340b318cb233d925cdb4c
c <c@gmail.com>
2024-11-27 14:17:22 +0100

    js menu change

--- menu.js
+++ menu.js
@@ -48,19 +48,6 @@ for (let i = 1; i <= 28; i++) {
     })
 }
 
-//Szűrés:
-
-const filter = document.querySelector('.bi-funnel');
-const filter0 = document.querySelector('#funnel');
-const types = document.querySelector('#types');
-const menuLine = document.querySelector('#menuline');
-filter0.addEventListener('click', event => {
-    types.classList.toggle('hidden');
-    menuLine.classList.toggle('hidden');
-    filter.classList.toggle('bi-funnel-fill');
-    filter.classList.toggle('bi-funnel');
-})
-
 const cart0 = document.querySelector('#cartOpen0');
 const cart = document.querySelector('#cartOpen');
 cart0.addEventListener('click', event => {
