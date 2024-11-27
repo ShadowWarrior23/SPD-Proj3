0255ef027daf422741ab98291dcb68cc03de7e25
Akos 7738 <czmorekakos@gmail.com>
2024-11-27 19:11:53 +0100

    putting the 2 pages together

---
+++ app.js
@@ -0,0 +1,50 @@
+const preloader = document.querySelector("#preloader");
+
+window.addEventListener("load", () => {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+})
+
+const navbar = document.querySelector("nav");
+const menuButton = document.querySelector("#menu-button")
+menuButton.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+})
+
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+})
+
+sr.reveal('.home-image',{delay:100, origin:'left'})
+sr.reveal('.about-text-content',{delay:100, origin:'right'})
+sr.reveal('#about-image',{delay:100, origin:'left'})
+sr.reveal('.slide-container', {delay:100, origin:'left'})
+
+
+new Swiper(".slide-container", {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    spaceBetween: 20, 
+    slidesPerView: 1, 
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
