fc79c8baf3dc18317fa164b3539fbc106a813539
c <c@gmail.com>
2024-12-11 13:47:44 +0100

    spin wheel edit

--- spin_wheel.js
+++ spin_wheel.js
@@ -1,6 +1,6 @@
 const wheel = document.querySelector(".wheel");
 const spinBtn = document.querySelector(".spinBtn");
-const value = Math.ceil(Math.random() *3600);
+let value = Math.ceil(Math.random() *3600);
 
 spinBtn.addEventListener("click", () => {
     wheel.style.transform = "rotate(" + value + "deg)";
